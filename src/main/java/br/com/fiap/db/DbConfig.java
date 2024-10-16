3bfb8b9bc8444c3d4d38329253317a3a553ac188
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-10-15 23:08:01 -0300

    criando configuração do banco de dados

---
+++ src/main/java/br/com/fiap/db/DbConfig.java
@@ -0,0 +1,28 @@
+package br.com.fiap.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConfig {
+
+    private final String url;
+    private final String user;
+    private final String password;
+
+
+    public DbConfig(String url, String user, String password) {
+        this.url = url;
+        this.user = user;
+        this.password = password;
+    }
+
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException("Connection error: " + e.getMessage());
+        }
+    }
+
+}
