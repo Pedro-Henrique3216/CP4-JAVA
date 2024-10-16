5e635ce04f479ac83bed8ba4d01a004f66a74aa3
ThiagoThmaz <kasuma17satto@gmail.com>
2024-10-16 11:21:46 -0300

    Ajustando: Main

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -1,24 +1,10 @@
 package br.com.fiap;
 
-import br.com.fiap.model.Pagamento;
-import br.com.fiap.model.Seguro;
-import br.com.fiap.model.SeguroImovelFactory;
-import br.com.fiap.model.TipoPagamento;
-import br.com.fiap.service.SeguroService;
+import br.com.fiap.service.ClienteService;
+import br.com.fiap.service.ClienteServiceImpl;
 
-import java.time.LocalDate;
-
-public class
-Main {
+public class Main {
     public static void main(String[] args) {
-       // System.out.println("Hello world!");
-        SeguroImovelFactory seguroImovelFactory = new SeguroImovelFactory();
-        Seguro seguro = seguroImovelFactory.createSeguro(null, null);
-        Pagamento pagamento = new Pagamento(seguro, TipoPagamento.DEBITO, LocalDate.now().minusDays(10));
-        pagamento.pagar();
-        System.out.println(pagamento.getStatusPagamento());
-
-
-
+        ClienteService clienteService = new ClienteServiceImpl();
     }
 }
