2dea0abce351adf04cf0b8cbd44903b83197f3ae
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-10-15 23:08:43 -0300

    criando dao de seguros

---
+++ src/main/java/br/com/fiap/db/dao/SeguroDao.java
@@ -0,0 +1,14 @@
+package br.com.fiap.db.dao;
+
+import br.com.fiap.model.Seguro;
+
+import java.util.List;
+
+public interface SeguroDao {
+
+    void insert(Seguro seguro);
+    void update(Seguro seguro);
+    void delete(Long id);
+    Seguro findById(Long id);
+    List<Seguro> findAll();
+}
---
+++ src/main/java/br/com/fiap/db/dao/SeguroDaoImp.java
@@ -0,0 +1,33 @@
+package br.com.fiap.db.dao;
+
+import br.com.fiap.model.Seguro;
+
+import java.util.List;
+
+public class SeguroDaoImp implements SeguroDao{
+
+    @Override
+    public void insert(Seguro seguro) {
+
+    }
+
+    @Override
+    public void update(Seguro seguro) {
+
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+
+    @Override
+    public Seguro findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Seguro> findAll() {
+        return List.of();
+    }
+}
